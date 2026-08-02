d30558041f99edd4e58ffc7cfcbdadff217ee3ed
Igor Balobanov <dinamsky@gmail.com>
2026-08-02 22:42:46 +0300

    Complete prayers and Big Book section

--- api/stage4.py
+++ api/stage4.py
@@ -1,12 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 import json
 import urllib.parse
-
 from api import stage3 as base
 
-
-PRAYERS = {
-    "serenity": (
-        "🙏 <b>Молитва о душевном покое</b>\n\n"
-        "Боже, дай мне разум и душевный покой принять то, что я не в силах изменить, "
-        "му
+PRAYERS={
+'serenity':'🙏 <b>Молитва о душевном покое</b>\n\nБоже, дай мне разум
